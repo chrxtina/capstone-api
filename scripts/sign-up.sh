95bc9798d745d349a84346a0d685d4fea8faa0e1
chrxtina <christina.ad.nguyen@gmail.com>
2016-11-04 19:14:26 -0400

    Edit port of host URL for curl request
    Edit port of host URL for curl request

--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-curl --include --request POST http://localhost:3000/sign-up \
+curl --include --request POST http://localhost:4741/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "an@example.email",
-      "password": "an example password",
-      "password_confirmation": "an example password"
+      "email": "cn@cn",
+      "password": "123",
+      "password_confirmation": "123"
     }
   }'
 
-curl --include --request POST http://localhost:3000/sign-up \
+curl --include --request POST http://localhost:4741/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "another@example.email",
-      "password": "an example password",
-      "password_confirmation": "an example password"
+      "email": "ab@ab",
+      "password": "123",
+      "password_confirmation": "123"
     }
   }'
