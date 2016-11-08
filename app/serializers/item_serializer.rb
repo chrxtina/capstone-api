b74625c96153b3ebad468ce9120ed43f7fe2967a
chrxtina <christina.ad.nguyen@gmail.com>
2016-11-07 23:36:33 -0500

    Add custom method (user) to ItemSerializer
    Had not included user in Item Serializer, so Ember was not happy
    not having access to the user's id

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,8 +1,12 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :location, :body, :address, :category
+  attributes :id, :title, :location, :body, :address, :category, :user
   has_one :user
 
   def category
     object.category_id
+  end
+
+  def user
+    object.user_id
   end
 end
