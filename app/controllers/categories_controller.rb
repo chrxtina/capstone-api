f6c84677a67867d5c782fcf548d4ff3d91d4b87d
chrxtina <christina.ad.nguyen@gmail.com>
2016-11-09 15:42:58 -0500

    Remove create & destroy from Categories Resources
    Purpose: Prevent users from create/deleting categories
    Left 'update' action bc creating an item will also update the categories
      item array.
    - Make Categories resource routes only index, show and update
    - Comment out create, destroy actions in CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :update, :destroy]
+  # before_action :set_category, only: [:show, :update, :destroy]
 
   # GET /categories
   # GET /categories.json
@@ -12,20 +12,22 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @category = Category.find(params[:id])
+
     render json: @category
   end
 
   # POST /categories
   # POST /categories.json
-  def create
-    @category = Category.new(category_params)
-
-    if @category.save
-      render json: @category, status: :created, location: @category
-    else
-      render json: @category.errors, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @category = Category.new(category_params)
+  #
+  #   if @category.save
+  #     render json: @category, status: :created, location: @category
+  #   else
+  #     render json: @category.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /categories/1
   # PATCH/PUT /categories/1.json
@@ -41,19 +43,19 @@ class CategoriesController < ApplicationController
 
   # DELETE /categories/1
   # DELETE /categories/1.json
-  def destroy
-    @category.destroy
-
-    head :no_content
-  end
+  # def destroy
+  #   @category.destroy
+  #
+  #   head :no_content
+  # end
 
   private
 
-    def set_category
-      @category = Category.find(params[:id])
-    end
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
-    def category_params
-      params.require(:category).permit(:name)
-    end
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
